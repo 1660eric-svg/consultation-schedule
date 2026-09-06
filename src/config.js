aeb5b4c2df277a317b6d8d44dafce8c491655299
1660eric-svg <1660eric@gmail.com>
2026-09-06 13:24:01 +0900

    Update Apps Script endpoint

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // Apps Script 배포 후 받은 웹 앱 URL(/exec)을 여기에 붙여 넣으세요.
-export const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz5fJwVXuIHwWjoEvas0TOvgJsyuStmnLDSKBaqgvLG_hVOYK-AflqyoD2PPFZPrLHhbA/exec'
+export const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzo2dRJJ7DdaxyNvdtukJOwLG3pkiXsyUBitd7fLHRu6eQoI033QXf3aAn_Bvu41n13ng/exec'
